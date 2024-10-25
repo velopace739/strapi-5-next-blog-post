8f93967ce4230bebe122baf7e00ef9332b430596
velopace739 <velopace7@gmail.com>
2024-10-25 11:13:41 +0800

    fetch data

--- client/src/app/our-team/page.tsx
+++ client/src/app/our-team/page.tsx
@@ -1,7 +1,36 @@
-export default function OurTeam() {
+import qs from 'qs'
+
+async function getTeamMembers() {
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const path = "/api/team-members";
+
+  const url = new URL(path, baseUrl);
+  url.search = qs.stringify({
+    populate: {
+      photo: {
+        fields: ['alternativeText', 'name', 'url']
+      }
+    }
+  })
+
+  const res = await fetch(url);
+
+  if (!res.ok) throw new Error("Failed to fetch team members");
+
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+}
+
+export default async function OurTeam() {
+  const teamMembers = await getTeamMembers();
+  console.log(teamMembers);
+
   return (
     <div>
       <h1>Our Team</h1>
+      <pre>{JSON.stringify(teamMembers, null, 2)} </pre>
     </div>
   );
 }
