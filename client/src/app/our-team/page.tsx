3461220372abbe24ac49b3603339a29d5a8e7f00
velopace739 <velopace7@gmail.com>
2024-10-25 11:21:59 +0800

    update team member page

--- client/src/app/our-team/page.tsx
+++ client/src/app/our-team/page.tsx
@@ -1,7 +1,9 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import qs from 'qs'
 
 async function getTeamMembers() {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL;
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:1337";
   const path = "/api/team-members";
 
   const url = new URL(path, baseUrl);
@@ -23,14 +25,64 @@ async function getTeamMembers() {
   return data;
 }
 
+interface TeamMemberProps {
+  id: number;
+  documentId: string;
+  name: string;
+  description: string;
+  slug: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  locale: string;
+  photo: {
+    id: number;
+    documentId: string;
+    alternativeText: string;
+    name: string;
+    url: string;
+  };
+}
+
+function TeamMemberCard({
+  name,
+  description,
+  photo,
+  slug,
+}: Readonly<TeamMemberProps>) {
+  const imageUrl = `${
+    process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:1337"
+  }${photo.url}`;
+  return (
+    <Link
+      href={`/our-team/${slug}`}
+      className="bg-white rounded-lg shadow-md overflow-hidden"
+    >
+      <Image
+        src={imageUrl}
+        alt={photo.alternativeText || name}
+        width={500}
+        height={500}
+      />
+      <div className="p-6">
+        <h3 className="text-xl font-semibold mb-2">{name}</h3>
+        <p className="text-gray-600">{description}</p>
+      </div>
+    </Link>
+  );
+}
+
 export default async function OurTeam() {
   const teamMembers = await getTeamMembers();
-  console.log(teamMembers);
 
   return (
     <div>
-      <h1>Our Team</h1>
-      <pre>{JSON.stringify(teamMembers, null, 2)} </pre>
+      <h1 className="text-3xl font-bold mb-8">Our Team</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {teamMembers.data.map((member: TeamMemberProps) => (
+          <TeamMemberCard key={member.documentId} {...member} />
+        ))}
+      </div>
     </div>
   );
 }
